961f8ed3cfc149464a90985664c2c1901c9bec81
Svetlin Georfiev <svetlin2002@gmail.com>
2024-03-20 04:55:34 +0200

    Safe and load feature, not yet fully implimented
    A work in progress for the safe and load feature of the game.

---
+++ GDD Minor/Assets/Lilac/LilacScripts/DataPersistenceManager.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class DataPersistenceManager : MonoBehaviour
+{
+
+    [SerializeField] private string fileName;
+
+    public static DataPersistenceManager instance { get; private set; }
+
+    private FileDataHandler dataHandler;
+    private GameData gameData;
+    private List<IDataPersistence> dataPersistenceObjects;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogError("More than one Data Persistence Manager in the scene");
+        }
+        instance = this;
+    }
+
+    private void Start()
+    {
+        this.dataHandler = new FileDataHandler(Application.persistentDataPath, fileName);
+        this.dataPersistenceObjects = FindAllDataPersistenceObjects();
+
+    }
+
+    public void NewGame()
+    {
+        this.gameData = new GameData();
+    }
+
+    public void LoadGame()
+    {
+        this.gameData = dataHandler.Load();
+
+        if(this.gameData == null)
+        {
+            NewGame();
+        }
+
+        foreach(IDataPersistence dataPersistenceObj in dataPersistenceObjects)
+        {
+            dataPersistenceObj.LoadData(gameData);
+        }
+    }
+
+    public void SaveGame()
+    {
+        foreach (IDataPersistence dataPersistenceObj in dataPersistenceObjects)
+        {
+            dataPersistenceObj.SaveData(ref gameData);
+        }
+        dataHandler.Save(gameData);
+    }
+
+    private List<IDataPersistence> FindAllDataPersistenceObjects()
+    {
+        IEnumerable<IDataPersistence> dataPersistenceObjects = FindObjectsOfType<MonoBehaviour>().OfType<IDataPersistence>();
+
+        return new List<IDataPersistence>(dataPersistenceObjects);
+    }
+}
