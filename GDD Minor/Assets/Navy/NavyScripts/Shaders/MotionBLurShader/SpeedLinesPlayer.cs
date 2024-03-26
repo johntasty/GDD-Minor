9c8a54fc1ae2b7cde075b842cc09e44b8fee075c
johntasty <johnezekiel93@gmail.com>
2024-03-26 15:22:32 +0100

    Value changing for fog

---
+++ GDD Minor/Assets/Navy/NavyScripts/Shaders/MotionBLurShader/SpeedLinesPlayer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.PostProcessing;
+
+public class SpeedLinesPlayer : MonoBehaviour
+{
+    [SerializeField]
+    VolumeProfile _PostEffect;
+   
+    CustomVolumeFeatureLines _FeatureLines;
+    CustomVolumeFeatureBlur  _FeatureBlur;
+   
+    Rigidbody _Rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _PostEffect.TryGet(out _FeatureLines);
+        _PostEffect.TryGet(out _FeatureBlur);
+        _Rigidbody = GetComponent<Rigidbody>();
+
+        _FeatureLines._LineDensity.value = 0f;
+        _FeatureBlur.intensity.value = 0f;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        _FeatureLines._LineDensity.value = _Rigidbody.velocity.magnitude;
+        _FeatureBlur.intensity.value = _Rigidbody.velocity.magnitude;
+    }
+}
