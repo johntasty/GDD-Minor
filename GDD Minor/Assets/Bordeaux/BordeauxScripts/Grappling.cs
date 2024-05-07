02ad32ead750917697e07613bbe29c6312ccbaef
Petros <peteran20@outlook.com>
2024-05-07 14:12:26 +0200

    Finished Version 1 Grappling Hook, created Player Prefab
    (Grappling hook isnt perfect, but works well if theres not a lot of grappling points close to each other)

--- GDD Minor/Assets/Bordeaux/BordeauxScripts/Grappling.cs
+++ GDD Minor/Assets/Bordeaux/BordeauxScripts/Grappling.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Grappling : MonoBehaviour
 {
     [Header("References")]
     private PeterMovement pm;
     public Transform cam;
+    public Camera playerCam;
     public Transform gunTip;
     public LayerMask whatIsGrappleable;
     public LineRenderer lr;
@@ -30,9 +32,15 @@ public class Grappling : MonoBehaviour
     
     private Collider grapplePointObjectCollider;
 
+    private List<GameObject> grapplePoints;
+
+    private Plane[] cameraFrustum;
+    
     private void Start()
     {
         pm = GetComponent<PeterMovement>();
+        playerCam = cam.GetComponent<Camera>();
+        grapplePoints = new List<GameObject>(GameObject.FindGameObjectsWithTag("GrapplePoint"));
     }
 
     private void Update()
@@ -41,12 +49,16 @@ public class Grappling : MonoBehaviour
 
         if (grapplingCdTimer > 0)
             grapplingCdTimer -= Time.deltaTime;
+
+        if (grapplePoints.Count == 0) return;
+        target = FindClosestGrapplePoint();
+
     }
 
     private void LateUpdate()
     {
-        // if (grappling)
-        //    lr.SetPosition(0, gunTip.position);
+        if (grappling)
+           lr.SetPosition(0, gunTip.position);
     }
 
     private void StartGrapple()
@@ -57,23 +69,28 @@ public class Grappling : MonoBehaviour
 
         pm.freeze = true;
 
-        RaycastHit hit;
-        if(Physics.Raycast(cam.position, cam.forward, out hit, maxGrappleDistance, whatIsGrappleable))
-        {
-            grapplePointObjectCollider = hit.collider;
-            grapplePoint = hit.point;
-    
-            Invoke(nameof(ExecuteGrapple), grappleDelayTime);
-        }
-        else
-        {
-            grapplePoint = cam.position + cam.forward * maxGrappleDistance;
+ 
+            Vector3 direction = (target.transform.position - cam.position).normalized;
+            RaycastHit hit;
+            if (IsVisible(playerCam, target) && Physics.Raycast(cam.position, direction, out hit, maxGrappleDistance, whatIsGrappleable))
+            {
+                Debug.Log(hit.collider.gameObject.name);
+                Debug.DrawLine(cam.position, hit.point, Color.red);
 
-            Invoke(nameof(StopGrapple), grappleDelayTime);
-        }
+                grapplePointObjectCollider = hit.collider;
+                grapplePoint = hit.point;
 
-        //lr.enabled = true;
-        //lr.SetPosition(1, grapplePoint);
+                Invoke(nameof(ExecuteGrapple), grappleDelayTime);
+            }
+            else
+            {
+                grapplePoint = cam.position + cam.forward * maxGrappleDistance;
+
+                Invoke(nameof(StopGrapple), grappleDelayTime);
+            }
+
+            lr.enabled = true;
+            lr.SetPosition(1, grapplePoint);
     }
 
     private void ExecuteGrapple()
@@ -95,17 +112,20 @@ public class Grappling : MonoBehaviour
 
     public void StopGrapple()
     {
+    if (grapplePointObjectCollider != null)
+    {
+        grapplePoints.Remove(grapplePointObjectCollider.gameObject);
         grapplePointObjectCollider.gameObject.SetActive(false);
-        
+        }
         pm.freeze = false;
 
         grappling = false;
 
         grapplingCdTimer = grapplingCd;
 
-        //lr.enabled = false;
+        lr.enabled = false;
     }
-
+ 
     public bool IsGrappling()
     {
         return grappling;
@@ -115,4 +135,44 @@ public class Grappling : MonoBehaviour
     {
         return grapplePoint;
     }
+    
+    public GameObject target;
+
+    private bool IsVisible(Camera c, GameObject target)
+    {
+        var planes = GeometryUtility.CalculateFrustumPlanes(c);
+        var point = target.transform.position;
+
+        foreach (var plane in planes)
+        {
+            if (plane.GetDistanceToPoint(point)< 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    GameObject FindClosestGrapplePoint()
+    {
+        GameObject closestObject = null;
+        float closestDistance = Mathf.Infinity;
+
+        // Iterate through all objects
+        foreach (GameObject obj in grapplePoints)
+        {
+            // Calculate the distance between the player and the object
+            float distance = Vector3.Distance(transform.position, obj.transform.position);
+
+            // Check if this object is closer than the current closest object
+            if (distance < closestDistance)
+            {
+                closestObject = obj;
+                closestDistance = distance;
+            }
+        }
+
+        return closestObject;
+    }
+
 }
