8b441d07228dbdd1a410b8abb3e12eff8b007419
johntasty <johnezekiel93@gmail.com>
2024-06-05 13:33:36 +0200

    Added ui

--- GDD Minor/Assets/Navy/NavyScripts/InteractionSystem/BackendScripts/On_Off.cs
+++ GDD Minor/Assets/Navy/NavyScripts/InteractionSystem/BackendScripts/On_Off.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class On_Off : MonoBehaviour
 {
-    [SerializeField] MonoBehaviour obj;
-    public void OnOff()
+    public void OnOff(MonoBehaviour obj)
     {
         obj.enabled = !obj.isActiveAndEnabled;
+    }
+    public void OnOffObject(GameObject go)
+    {
+        bool active = go.activeInHierarchy;
+        go.SetActive(!active);
+    }
+    public void CursorOn_Off()
+    {
+        Cursor.lockState = Cursor.visible ? CursorLockMode.None : CursorLockMode.Locked;
+        Cursor.visible = !Cursor.visible;
     }
 }
